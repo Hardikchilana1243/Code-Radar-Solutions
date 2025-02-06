07870ccdd51d62ca4d3f316541b77bb2e8e9a418
Hardikchilana1243 <h2154678@gmail.com>
2025-02-06 16:27:27 +0530

    Solution to Input and Print a Double

--- Input and Print a Double@@6788d5012db25efec946f942/code.c
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -3,7 +3,7 @@ int main(){
     int num,doubled ;
     printf("enter the number : ");
     scanf("%d",&num);
-    doubled = num*2;
-    printf("the double of the number is : %d",doubled);
+    doubled = num *2;
+    printf("double of %d is %d\n",num,doubled);
     return 0;
 }
