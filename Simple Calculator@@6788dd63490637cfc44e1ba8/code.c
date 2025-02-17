fe096049c4dc6587fcc4b6891479ce4d3172eede
Hardik Chilana <h2154678@gmail.com>
2025-02-18 01:41:49 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -9,11 +9,11 @@ int  main(){
     {
         printf("%d",sub);
     }
-    else if(mul = a*b)
+    else if(mul == a*b)
     {
         printf("%d",mul);
     }
-    else if(div = a/b){
+    else if(div == a/b){
         printf("%d",div);
     }
     else {
