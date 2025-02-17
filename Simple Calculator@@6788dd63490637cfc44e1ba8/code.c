c46d0075796002ca212cd76035a3045cf4716aa9
Hardik Chilana <h2154678@gmail.com>
2025-02-18 01:42:43 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,18 +2,18 @@
 int  main(){
     int a,b,sum,sub,div,mul;
     scanf("%d%d",&a,&b);
-    if(sum == a+b){
+    if(sum = a+b){
         printf("%d",sum);
     }
-    else if(sub = a-b)
+    if(sub = a-b)
     {
         printf("%d",sub);
     }
-    else if(mul == a*b)
+    if(mul = a*b)
     {
         printf("%d",mul);
     }
-    else if(div == a/b){
+    if(div = a/b){
         printf("%d",div);
     }
     else {
