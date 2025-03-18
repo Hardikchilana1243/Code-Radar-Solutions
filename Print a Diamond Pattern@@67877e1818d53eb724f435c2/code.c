87dc84b9932c4fabfb5521fdea5b34f4cb4300a8
Hardik Chilana <h2154678@gmail.com>
2025-03-18 18:25:16 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,27 +1,63 @@
-#include<stdio.h>
-int main(){
-    int n;
-    // printf("enter the number of lines: ");
-    scanf("%d",&n);
-    int nsp= n/2;
-    int nst = 1;
-    int ml =  n/2+1;
-    for(int i  =1;i<=n;i++){
-        for(int j=1;j<=nsp;j++){
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     // printf("enter the number of lines: ");
+//     scanf("%d",&n);
+//     int nsp= n/2;
+//     int nst = 1;
+//     int ml =  n/2+1;
+//     for(int i  =1;i<=n;i++){
+//         for(int j=1;j<=nsp;j++){
+//             printf(" ");
+//         }
+//         for(int k=1;k<=nst;k++){
+//             printf("*");
+//         }
+//         if(i<ml){
+//             nsp--;
+//             nst+=2;
+//         }
+//         else{
+//             nsp++;
+//             nst-=2;
+//         }
+//         printf("\n");
+//         }
+//         return 0;
+//     }
+
+
+#include <stdio.h>
+
+void printDiamond(int n) {
+    // Upper half
+    for (int i = 1; i <= n; i += 2) {
+        for (int j = 0; j < (n - i) / 2; j++)
             printf(" ");
-        }
-        for(int k=1;k<=nst;k++){
+        for (int j = 0; j < i; j++)
             printf("*");
-        }
-        if(i<ml){
-            nsp--;
-            nst+=2;
-        }
-        else{
-            nsp++;
-            nst-=2;
-        }
         printf("\n");
-        }
-        return 0;
     }
+
+    // Lower half
+    for (int i = n - 2; i >= 1; i -= 2) {
+        for (int j = 0; j < (n - i) / 2; j++)
+            printf(" ");
+        for (int j = 0; j < i; j++)
+            printf("*");
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter an odd number for diamond size: ");
+    scanf("%d", &n);
+
+    if (n % 2 == 0) {
+        printf("Please enter an odd number.\n");
+    } else {
+        printDiamond(n);
+    }
+    return 0;
+}
