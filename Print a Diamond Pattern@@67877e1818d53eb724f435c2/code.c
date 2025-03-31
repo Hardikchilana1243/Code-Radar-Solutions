e6d7e7985a258c002e8c91b4a7f9be0d7fc6286d
Hardik Chilana <h2154678@gmail.com>
2025-03-31 15:22:38 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,54 +1,54 @@
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     // printf("enter the number of lines: ");
-//     scanf("%d",&n);
-//     int nsp= n/2;
-//     int nst = 1;
-//     int ml =  n/2+1;
-//     for(int i  =1;i<=n;i++){
-//         for(int j=1;j<=nsp;j++){
+#include<stdio.h>
+int main(){
+    int n;
+    // printf("enter the number of lines: ");
+    scanf("%d",&n);
+    int nsp= n/2;
+    int nst = 1;
+    int ml =  n/2+1;
+    for(int i  =1;i<=n;i++){
+        for(int j=1;j<=nsp;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=nst;k++){
+            printf("*");
+        }
+        if(i<ml){
+            nsp--;
+            nst+=2;
+        }
+        else{
+            nsp++;
+            nst-=2;
+        }
+        printf("\n");
+        }
+        return 0;
+    }
+
+
+// #include <stdio.h>
+
+// void printDiamond(int n) {
+//     // Upper half
+//     for (int i = 1; i <= n; i += 2) {
+//         for (int j = 0; j < (n - i) / 2; j++)
 //             printf(" ");
-//         }
-//         for(int k=1;k<=nst;k++){
+//         for (int j = 0; j < i; j++)
 //             printf("*");
-//         }
-//         if(i<ml){
-//             nsp--;
-//             nst+=2;
-//         }
-//         else{
-//             nsp++;
-//             nst-=2;
-//         }
 //         printf("\n");
-//         }
-//         return 0;
 //     }
 
-
-#include <stdio.h>
-
-void printDiamond(int n) {
-    // Upper half
-    for (int i = 1; i <= n; i += 2) {
-        for (int j = 0; j < (n - i) / 2; j++)
-            printf(" ");
-        for (int j = 0; j < i; j++)
-            printf("*");
-        printf("\n");
-    }
-
-    // Lower half
-    for (int i = n - 2; i >= 1; i -= 2) {
-        for (int j = 0; j < (n - i) / 2; j++)
-            printf(" ");
-        for (int j = 0; j < i; j++)
-            printf("*");
-        printf("\n");
-    }
-    return 0;
-}
+//     // Lower half
+//     for (int i = n - 2; i >= 1; i -= 2) {
+//         for (int j = 0; j < (n - i) / 2; j++)
+//             printf(" ");
+//         for (int j = 0; j < i; j++)
+//             printf("*");
+//         printf("\n");
+//     }
+//     return 0;
+// }
 
 // int main() {
 //     int n;
